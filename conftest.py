68ea8e60246d89a614c81e144134e5d18af9fc0f
Сидоров Иван Сергеевич <I.Sidorov@norbit.ru>
2023-12-30 16:16:37 +0300

    selenium v4

--- conftest.py
+++ conftest.py
@@ -1,15 +1,14 @@
 import pytest
 
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service as chromeService
+from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
+
+
 
 @pytest.fixture(scope="function")
 def driver():
-    options = webdriver.ChromeOptions()
-    # options.add_argument("-headless")
-    driver_service = chromeService(ChromeDriverManager().install())
-    driver = webdriver.Chrome(service=driver_service, options=options)
+    driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
     driver.maximize_window()
     yield driver
     driver.quit()
--- tests/test_elements.py
+++ tests/test_elements.py
@@ -4,6 +4,6 @@ import time
 from pages.base_page import BasePage
 
 def test(driver):
-    page = BasePage(driver, 'google.com')
+    page = BasePage(driver, "http://google.com")
     page.open()
     time.sleep(3)
