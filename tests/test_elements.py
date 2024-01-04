6e933cb5058386881f547dbf37d3f46afbb1ec3d
AlmostIvanSidorov <vanosidorov@gmail.com>
2024-01-04 16:04:21 +0300

    final elements tests

--- tests/test_elements.py
+++ tests/test_elements.py
@@ -5,12 +5,12 @@ class TestElements:
         def test_elements(self, driver):
             text_box_page = TextBoxPage(driver, "https://demoqa.com/text-box")
             text_box_page.open()
-            text_box_page.fill_all_fields()
+            input_full_name, input_email, input_current_address, input_permanent_address = text_box_page.fill_all_fields()
 
             output_full_name, output_email, output_current_address, output_permanent_address = text_box_page.check_field_form()
 
-            assert output_full_name == text_box_page.input_full_name, "wrong full name"
-            assert output_email == text_box_page.input_email, "wrong email"
-            assert output_current_address == text_box_page.input_current_address, "wrong current address"
-            assert output_permanent_address == text_box_page.input_permanent_address, "wrong permanent address"
+            assert output_full_name == input_full_name, "the full name doesn't match"
+            assert output_email == input_email, "the email doesn't match"
+            assert output_current_address == input_current_address, "the current address doesn't match"
+            assert output_permanent_address == input_permanent_address, "the permanent address doesn't match"
             
