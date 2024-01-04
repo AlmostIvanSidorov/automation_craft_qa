eafd8cd34ec22da31498e5dba1c467345aec5d42
AlmostIvanSidorov <vanosidorov@gmail.com>
2024-01-04 16:04:21 +0300

    final elements tests

--- pages/elements_page.py
+++ pages/elements_page.py
@@ -1,25 +1,29 @@
 import time
 
 from locators.elements_page_locators import TextBoxPageLocators as locators
+from generator.generator import generated_person
 from pages.base_page import BasePage
 
 class TextBoxPage(BasePage):
-    input_full_name = "admin"
-    input_email = "admin@email.com"
-    input_current_address = "some street"
-    input_permanent_address = "same street"
 
     def __init__(self, driver, url):
         super().__init__(driver, url)
 
     def fill_all_fields(self):
-        self.element_is_visible(locators.FULL_NAME).send_keys(self.input_full_name)
-        self.element_is_visible(locators.EMAIL).send_keys(self.input_email)
-        self.element_is_visible(locators.CURRENT_ADDRESS).send_keys(self.input_current_address)
-        self.element_is_visible(locators.PERMANENT_ADDRESS).send_keys(self.input_permanent_address)
+        person_info = next(generated_person())
+        input_full_name = person_info.full_name
+        input_email = person_info.email
+        input_current_address = person_info.current_address
+        input_permanent_address = person_info.permanent_address
+        self.element_is_visible(locators.FULL_NAME).send_keys(input_full_name)
+        self.element_is_visible(locators.EMAIL).send_keys(input_email)
+        self.element_is_visible(locators.CURRENT_ADDRESS).send_keys(input_current_address)
+        self.element_is_visible(locators.PERMANENT_ADDRESS).send_keys(input_permanent_address)
         self.element_is_visible(locators.SUBMIT_BUTTON).click()
 
         time.sleep(2)
+
+        return input_full_name, input_email, input_current_address, input_permanent_address
 
     def check_field_form(self):
         full_name = self.element_is_present(locators.CREATED_FULL_NAME).text.split(":")[1]
