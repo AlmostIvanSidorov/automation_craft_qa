4ad2960f988cdb0bfab49d3f5951626ae53dbcc6
AlmostIvanSidorov <vanosidorov@gmail.com>
2024-01-07 09:57:28 +0300

    textbox fix

--- tests/test_elements.py
+++ tests/test_elements.py
@@ -2,12 +2,12 @@ from pages.elements_page import TextBoxPage
 
 class TestElements:
     class TestTextBox:
-        def test_elements(self, driver):
-            text_box_page = TextBoxPage(driver, "https://demoqa.com/text-box")
-            text_box_page.open()
-            input_full_name, input_email, input_current_address, input_permanent_address = text_box_page.fill_all_fields()
+        def test_textbox(self, driver):
+            textbox_page = TextBoxPage(driver, "https://demoqa.com/text-box")
+            textbox_page.open()
+            input_full_name, input_email, input_current_address, input_permanent_address = textbox_page.fill_all_fields()
 
-            output_full_name, output_email, output_current_address, output_permanent_address = text_box_page.check_field_form()
+            output_full_name, output_email, output_current_address, output_permanent_address = textbox_page.check_field_form()
 
             assert output_full_name == input_full_name, "the full name doesn't match"
             assert output_email == input_email, "the email doesn't match"
