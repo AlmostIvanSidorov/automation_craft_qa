6fde6a67d1b680fa087da67960b79299a05ecb11
AlmostIvanSidorov <vanosidorov@gmail.com>
2024-02-04 01:14:30 +0300

    browser-windows tests were added

--- tests/test_alerts_frame_windows_page.py
+++ tests/test_alerts_frame_windows_page.py
@@ -9,11 +9,11 @@ class TestAlertsFrameWindow:
             browser_window_page = BrowserWindowPage(driver, "https://demoqa.com/browser-windows")
             browser_window_page.open()
             test_result = browser_window_page.checked_opened_new_tab()
-            assert test_result == 'This is a sample page', 'the new tab has not opened or incorrect tab has been opened'
+            assert test_result == 'This is a sample page', 'the new tab has not been opened or incorrect tab has been opened'
 
         def test_new_window(self, driver):
             browser_window_page = BrowserWindowPage(driver, "https://demoqa.com/browser-windows")
             browser_window_page.open()
             test_result = browser_window_page.checked_opened_new_window()
-            assert test_result == 'This is a sample page', 'the new window has not opened or incorrect window has been opened'
+            assert test_result == 'This is a sample page', 'the new window has not been opened or incorrect window has been opened'
 
